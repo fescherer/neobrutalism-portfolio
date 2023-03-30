7f29a731f4f5c15eb0997740b22e14859687fb62
Felipe Scherer <felipescherer25@gmail.com>
2023-03-30 13:12:13 -0300

    :sparkles: feat: history components

--- src/@types/translations/AboutTranslation.ts
+++ src/@types/translations/AboutTranslation.ts
@@ -1,10 +1,24 @@
 export type AboutTranslation = {
   title: string
+  first_description: string
   introduction: IntroductionAboutTranslation
+  my_history: TypeMyHistory
 }
 
 export type IntroductionAboutTranslation = {
-  'skills-experience': string
-  'my-history': string
-  'alt-image': string
+  skills_experience: string
+  my_history: string
+  alt_image: string
+}
+
+export type TypeMyHistory = {
+  my_history: TextBlock
+  born: TextBlock
+  studies: TextBlock
+  future: TextBlock
+}
+
+export type TextBlock = {
+  title: string
+  text: string
 }
